b039c99d930434e3d69fb2ba307cb1ac9cc28381
Tung Tran <tungts1101@gmail.com>
2023-12-28 01:35:23 +0900

    Install dependencies separately.

---
+++ README.md
@@ -0,0 +1,5 @@
+## Installation
+Requirement dependencies
+```
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu118
+```
--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ setup(
     name="pytorch_trainer",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["torch==2.1.1@https://download.pytorch.org/whl/cu118", 
-                      "torchvision==0.16.1@https://download.pytorch.org/whl/cu118"],
+    install_requires=[],
 )

0f280acf234a18994d9cf2e9c61ff68d28fe985e
Tung Tran <tungts1101@gmail.com>
2023-12-28 01:17:06 +0900

    Fix error unnecessary to define pytorch-cuda.

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="pytorch_trainer",
     version="0.0.1",
     packages=find_packages(),
-    install_requires=["torch==2.1.1", "torchvision==0.16.1", "pytorch-cuda==11.8"],
+    install_requires=["torch==2.1.1", "torchvision==0.16.1"],
 )
